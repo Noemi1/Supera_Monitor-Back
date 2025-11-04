93bf50c5c3c6c6677f60b0c98e02456b714da25a
Noemi1 <calmeida.no@gmail.com>
2025-11-04 14:43:19 -0300

    UPDATE: AlunoVigenciaList com novas propriedades de professor

--- Supera_Monitor-Back/Entities/Views/AlunoVigenciaList.cs
+++ Supera_Monitor-Back/Entities/Views/AlunoVigenciaList.cs
@@ -7,11 +7,17 @@
 		public int Aluno_Id { get; set; }
 		
 		public string Aluno { get; set; } = String.Empty;
-		
+
 		public int Turma_Id { get; set; }
-		
+
 		public string Turma { get; set; } = String.Empty;
+
+		public int Professor_Id { get; set; }
+
+		public string Professor { get; set; } = String.Empty;
 		
+		public string CorLegenda { get; set; } = String.Empty;
+
 		public DateTime DataInicioVigencia { get; set; }
 		
 		public DateTime? DataFimVigencia { get; set; }
